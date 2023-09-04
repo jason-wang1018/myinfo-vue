949cd8bb3c4f9bee0fb00ebc464b841a7c00e586
jason_wang <wangdi101843@gmail.com>
2023-09-04 10:36:32 +0800

    路径配置

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,11 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  //服务器设置
   server: {
     open: true,
     port: 9420,
+    origin:'0.0.0.0',
     proxy: {
       '/api': {
         target: 'https://www.fastmock.site/mock/935d86475772276cc0f2649e50c82123',
@@ -19,9 +21,17 @@ export default defineConfig({
       }
     }
   },
+  //路径别名设置
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@a': fileURLToPath(new URL('./src/api', import.meta.url)),
+      '@c': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@h': fileURLToPath(new URL('./src/hooks', import.meta.url)),
+      '@r': fileURLToPath(new URL('./src/router', import.meta.url)),
+      '@s': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@u': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      '@v': fileURLToPath(new URL('./src/views', import.meta.url)),
     }
   }
 })
