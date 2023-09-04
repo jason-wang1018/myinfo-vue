9e607327f6764283efde0273a0fbbe6b21732fe0
jason_wang <wangdi101843@gmail.com>
2023-09-04 14:27:03 +0800

    element-plus的安装和全局配置

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,24 @@ import  './assets/rest.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+//element-plus的全局导入
+import ElementPlus from 'element-plus'
+//element-plus全局样式
+import 'element-plus/dist/index.css'
+//element-push国际化中文
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+
+app.use(ElementPlus, {locale: zhCn,})
+
 app.use(createPinia())
+
 app.use(router)
 
 app.mount('#app')
